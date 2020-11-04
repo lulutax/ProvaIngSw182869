7e5be67705ff18bcde6875b45ac89d56f8a22efc
lulutax <lulutassone97@gmail.com>
2020-11-04 12:19:26 +0100

    commit on branch dev

--- prova182869/src/main/java/it/unical/ingsw/provaIngSw/prova182869/App.java
+++ prova182869/src/main/java/it/unical/ingsw/provaIngSw/prova182869/App.java
@@ -1,13 +1,27 @@
 package it.unical.ingsw.provaIngSw.prova182869;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+
     public static void main( String[] args )
+    
     {
-        System.out.println( "Hello World!" );
+        MyListUtil m = new  MyListUtil();
+    	ArrayList l = new ArrayList(Arrays.asList(2,3,5,1,4));
+    	
+    	
+    	System.out.println(l);
+    	ArrayList<Integer> prova = m.sortArray(l, false);
+
+    
+        System.out.println( prova );
     }
 }
---
+++ prova182869/src/main/java/it/unical/ingsw/provaIngSw/prova182869/MyListUtil.java
@@ -0,0 +1,67 @@
+package it.unical.ingsw.provaIngSw.prova182869;
+
+import java.util.ArrayList;
+
+public class MyListUtil {
+
+	
+	
+	
+	public ArrayList<Integer> sortArray(ArrayList<Integer> l, boolean t){
+	
+		
+		//se è true ordinamento crescente
+		if(t== true) {
+			int index=0;
+			int min=0;
+			for( int i =0; i< l.size();i++) {
+				min = l.get(i);
+				for (int j=i+1; j< l.size();j++) {
+					
+					if(min >l.get(j)) {
+						
+						
+						min= l.get(j);
+						index = j;
+					}
+					
+				}
+				l.set(index, l.get(i));
+
+				l.set(i, min);
+				
+				
+			}
+			
+		}else {
+			
+			int index=0;
+			int max=0;
+			for( int i =0; i< l.size();i++) {
+				max = l.get(i);
+				for (int j=i+1; j< l.size();j++) {
+					
+					if(max <l.get(j)) {
+						
+						
+						max= l.get(j);
+						index = j;
+					}
+					
+				}
+				l.set(index, l.get(i));
+
+				l.set(i, max);
+				
+				
+			}
+			
+		}
+		
+		
+		return l;
+		
+		
+		
+	}
+}
