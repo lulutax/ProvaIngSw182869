14141fa53899a32cdaae55ad413ce82b53840402
lulutax <lulutassone97@gmail.com>
2020-11-04 11:59:40 +0100

    first commit

---
+++ prova182869/src/main/java/it/unical/ingsw/provaIngSw/prova182869/App.java
@@ -0,0 +1,13 @@
+package it.unical.ingsw.provaIngSw.prova182869;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
